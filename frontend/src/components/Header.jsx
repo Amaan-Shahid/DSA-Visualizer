5f14f4a85a3ac6f65192ebd220e201102948d094
amaan-shahid <arainamaan08@gmail.com>
2025-10-12 20:41:38 +0500

    Added Links to Buttons

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../styles/header.css'
 import arrow from "../assets/arrow.svg"
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='header-div'>
       <h2 className='header-heading'>Master Data Structures & Algorithms</h2>
       <p className='header-para'>Interactive visualizations that bring algorithms to life. Watch, learn, and understand how they work step-by-step.</p>
-      <button className="header-btn">Start Visualizing <img src={arrow} alt="" /></button>
+      <Link className='link' to="/visualizer"><button className="header-btn">Start Visualizing <img src={arrow} alt="" /></button></Link>
     </div>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import '../styles/Navbar.css'
 import moon from "../assets/theme-toggle.svg"
 import logo from "../assets/logo.png"
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='navbar-div'>
+      <Link to='/' className='link'>
       <div className="navbar-logo">
         <img src={logo} alt="" className="logo" />
         <h1>DSA Visualizer</h1>
       </div>
+      </Link>
       <div className="navbar-menu">
         <div className="theme-toggle-wrapper">
             <img className='theme-toggle' src={moon} alt="" />
